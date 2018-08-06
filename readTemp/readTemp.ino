926c810e571c752efed8d4b5af112adbc060b071
[Paul Leary] <leary@Pauls-MacBook-Pro.local>
2018-08-06 14:27:06 -0700

    working temp control, rough

--- readTemp/readTemp.ino
+++ readTemp/readTemp.ino
@@ -1,28 +1,70 @@
 int temperaturePin = A0;
 
+int knobPin = A2;
+
+int fanPin = 4;
+
 void setup() {
 
-  pinMode(temperaturePin, INPUT);
+  pinMode(fanPin, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   
 //  int temperature_analog_avg = analogRead(temperaturePin);
-
+//Serial.println(temperature_analog_avg);
   int temperature_analog_avg = read_temp_avg(temperaturePin, 20);
 
   float temperature_resistance_ohms = calculate_resistance(temperature_analog_avg, 10000.0, 5.0);
 
-  float temperature_C = calculate_temperature_C(temperature_resistance_ohms, 10000.0);
+//  Serial.println(temperature_resistance_ohms);
+
+  float temperature_C = calculate_temperature_C(temperature_resistance_ohms);
+
+  int knob_analog_avg = read_temp_avg(knobPin, 20);
+
+  float setpoint_C = knob2temp_C(knob_analog_avg);
+
+  bool fanState = (temperature_C > setpoint_C);
+
+  String fanString = "Off";
+  if(fanState){
+    fanString = "On";
+  }
 
   Serial.print("Temperature = ");
+//   Serial.print(temperature_resistance_ohms);
   Serial.print(temperature_C);
-//  Serial.write(char(176));
+////  Serial.write(char(176));
   Serial.write(0xC2);
   Serial.write(0xB0);
-  Serial.println("C");
+  Serial.print("C ");
+
+    Serial.print("Setpoint = ");
+//   Serial.print(temperature_resistance_ohms);
+  Serial.print(setpoint_C);
+////  Serial.write(char(176));
+  Serial.write(0xC2);
+  Serial.write(0xB0);
+  Serial.print("C");
+
+  Serial.print("  Fan = ");
+//   Serial.print(temperature_resistance_ohms);
+  Serial.print(fanString);
+////  Serial.write(char(176));
+//  Serial.write(0xC2);
+//  Serial.write(0xB0);
+  Serial.println();
+
+  digitalWrite(fanPin, fanState);
+  
 
 
+//while(1){
+//  
+//}
   delay(500);
 
 }
@@ -40,25 +82,41 @@ int read_temp_avg(int pin, int samples){
 }
 
 float calculate_resistance(int temp_analog, float series_resistance_ohms, float V_in){
-
+  float resolution_conversion = 0.0048828125;
+//Serial.println(resolution_conversion, 20);
 //  float resTemp = (0.000125*tSens[2]*(float)avgTemp)/(5-(0.000125*(float)avgTemp));
+//9.765625000000000e-04
+//Serial.println(temp_analog);
+//Serial.println((float)temp_analog);
   
-  float temp_resistance = (0.000125*series_resistance_ohms*(float)temp_analog)/(V_in-(0.000125*(float)temp_analog));
-
+  float temp_resistance = (resolution_conversion*series_resistance_ohms*(float)temp_analog)/(V_in-(resolution_conversion*(float)temp_analog));
+  
   return temp_resistance; 
   
   
 }
 
-float calculate_temperature_C(float temp_resistance, float series_resistance_ohms){
-  
-//  currentTemp = (1/(tSens[0]+(tSens[1]*log(resTemp))))-273.15;
+float calculate_temperature_C(float temp_resistance){
 
-  float temp_C = (1/(series_resistance_ohms+(3950*log(temp_resistance))))-273.15;
+//  Serial.println(temp_resistance);
+  
+//  float temp_C = (1/(((1/298.15)-((1/3950)*log(10000)))+((1/3950)*log(temp_resistance))))-273.15;
+
+  float temp_C = (1/(((0.003354016434681)-((0.0002531645569620253)*log(10000)))+((0.0002531645569620253)*log(temp_resistance))))-273.15;
+
+//  float temp_C = (1/(series_resistance_ohms+(3950*log(temp_resistance))))-273.15;
 
   return temp_C;
 
 
+}
+
+float knob2temp_C(int temp_analog){
+  float temp_min = 20;
+  float temp_max = 30;
+
+  float set_temp_C = temp_min + ((temp_max - temp_min)/1024)*temp_analog;
+  return set_temp_C;
 }
 
 //avgTemp is just the average value of 20 ADC readings for the thermistor.
